1ec494b1592dae8442d7b1c3edc7dff2a7ca3f1a
Hubert.Karw <hubert_karwowski@o2.pl>
2025-05-30 18:08:10 +0200

    java/github-proxy/feature/1

--- src/main/java/com/hubertkarw/github_proxy/feignClient/GithubRepositoryClient.java
+++ src/main/java/com/hubertkarw/github_proxy/feignClient/GithubRepositoryClient.java
@@ -2,6 +2,6 @@ package com.hubertkarw.github_proxy.feignClient;
 
 import org.springframework.cloud.openfeign.FeignClient;
 
-@FeignClient(value = "github", url = "https://gi")
+@FeignClient(value = "github", url = "https://api.github.com")
 public class GithubRepositoryClient {
 }
