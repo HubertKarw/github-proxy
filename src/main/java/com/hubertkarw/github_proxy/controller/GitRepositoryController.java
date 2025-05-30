46d9e9b5c4d35e1c9353342c180b626d834bb05d
Hubert.Karw <hubert_karwowski@o2.pl>
2025-05-30 18:14:44 +0200

    java/github-proxy/feature/1

--- src/main/java/com/hubertkarw/github_proxy/controller/GitRepositoryController.java
+++ src/main/java/com/hubertkarw/github_proxy/controller/GitRepositoryController.java
@@ -1,4 +1,13 @@
 package com.hubertkarw.github_proxy.controller;
 
+import com.hubertkarw.github_proxy.service.GitRepositoryService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/repos")
 public class GitRepositoryController {
+    private final GitRepositoryService service;
 }
--- src/main/java/com/hubertkarw/github_proxy/service/GitRepositoryService.java
+++ src/main/java/com/hubertkarw/github_proxy/service/GitRepositoryService.java
@@ -1,4 +1,12 @@
 package com.hubertkarw.github_proxy.service;
 
+import com.hubertkarw.github_proxy.repository.GitRepositoryJpaRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
 public class GitRepositoryService {
+    private final GitRepositoryJpaRepository repository;
 }
